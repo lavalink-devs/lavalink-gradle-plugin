1bd9eb10d08cda72c8f4ef66d7e6d8d6bddc9106
Michael Rittmeister <michael@rittmeister.in>
2023-07-04 00:47:52 +0200

    Fix bug causing manifest file not to be created

--- src/main/kotlin/tasks/GeneratePluginPropertiesTask.kt
+++ src/main/kotlin/tasks/GeneratePluginPropertiesTask.kt
@@ -3,18 +3,17 @@ package dev.arbjerg.lavalink.gradle.tasks
 import dev.arbjerg.lavalink.gradle.extension
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.properties
 import java.nio.file.Path
+import java.nio.file.StandardOpenOption
 import java.util.*
 import kotlin.io.path.bufferedWriter
+import kotlin.io.path.createDirectories
 import kotlin.io.path.div
 
-internal val Project.generatedPluginManifest: Provider<Path>
-    get() = extension.name.map { name ->
-        project.buildDir.toPath() / "generated" / "lavalink" / "main" / "resources"
-    }
+internal val Project.generatedPluginManifest: Path
+    get() = project.buildDir.toPath() / "generated" / "lavalink" / "main" / "resources"
 
 abstract class GeneratePluginPropertiesTask : DefaultTask() {
 
@@ -38,8 +37,9 @@ abstract class GeneratePluginPropertiesTask : DefaultTask() {
             set("path", extension.path.get())
         }
 
-        val file = project.generatedPluginManifest.get() / "lavalink-plugins" / "${extension.name.get()}.properties"
-        file.bufferedWriter().use { writer ->
+        val file = project.generatedPluginManifest / "lavalink-plugins" / "${extension.name.get()}.properties"
+        file.parent.createDirectories()
+        file.bufferedWriter(options = arrayOf(StandardOpenOption.CREATE)).use { writer ->
             properties.store(writer, null)
         }
     }
