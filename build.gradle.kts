8f2436b343ba07b83e4d8e57c2d594ddb210ef8f
Kamilake <exjang0@gmail.com>
2024-06-10 12:41:01 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 kotlin {
     jvmToolchain(11)
 }
